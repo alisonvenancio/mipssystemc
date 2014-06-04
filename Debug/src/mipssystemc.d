6d39ce5d056f5f53786ad40416a02f554613bb5a
Alison Venâncio <alison.venancio@gmail.com>
2014-06-04 12:22:02 -0400

    Esqueleto do projeto

---
+++ Debug/src/mipssystemc.d
@@ -0,0 +1,346 @@
+src/mipssystemc.d: ../src/mipssystemc.cpp \
+ /home/developer/.systemc/include/systemc.h \
+ /home/developer/.systemc/include/systemc \
+ /home/developer/.systemc/include/sysc/kernel/sc_cmnhdr.h \
+ /home/developer/.systemc/include/sysc/packages/boost/config.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/user.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/select_compiler_config.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/compiler/gcc.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/select_stdlib_config.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/stdlib/libstdcpp3.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/select_platform_config.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/platform/linux.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/posix_features.hpp \
+ /home/developer/.systemc/include/sysc/packages/boost/config/suffix.hpp \
+ /home/developer/.systemc/include/sysc/kernel/sc_externs.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_except.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_module.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_kernel_ids.h \
+ /home/developer/.systemc/include/sysc/utils/sc_report.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_except.h \
+ /home/developer/.systemc/include/sysc/utils/sc_report_handler.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_process.h \
+ /home/developer/.systemc/include/sysc/utils/sc_iostream.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_constants.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_object.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_attribute.h \
+ /home/developer/.systemc/include/sysc/communication/sc_export.h \
+ /home/developer/.systemc/include/sysc/communication/sc_communication_ids.h \
+ /home/developer/.systemc/include/sysc/communication/sc_interface.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_module_name.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_sensitive.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_time.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_nbdefs.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_cmnhdr.h \
+ /home/developer/.systemc/include/sysc/datatypes/fx/scfx_ieee.h \
+ /home/developer/.systemc/include/sysc/datatypes/fx/sc_fxdefs.h \
+ /home/developer/.systemc/include/sysc/utils/sc_machine.h \
+ /home/developer/.systemc/include/sysc/packages/boost/detail/endian.hpp \
+ /home/developer/.systemc/include/sysc/datatypes/fx/sc_fx_ids.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_nbutils.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bit_ids.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_int_ids.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_wait.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_simcontext.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_status.h \
+ /home/developer/.systemc/include/sysc/utils/sc_hash.h \
+ /home/developer/.systemc/include/sysc/utils/sc_pq.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_wait_cthread.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_logic.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_macros.h \
+ /home/developer/.systemc/include/sysc/utils/sc_mempool.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bit.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_ifs.h \
+ /home/developer/.systemc/include/sysc/communication/sc_writer_policy.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_process_handle.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_module.h \
+ /home/developer/.systemc/include/sysc/utils/sc_list.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_simcontext.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_process_handle.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_ver.h \
+ /home/developer/.systemc/include/sysc/communication/sc_buffer.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal.h \
+ /home/developer/.systemc/include/sysc/communication/sc_port.h \
+ /home/developer/.systemc/include/sysc/kernel/sc_event.h \
+ /home/developer/.systemc/include/sysc/communication/sc_prim_channel.h \
+ /home/developer/.systemc/include/sysc/tracing/sc_trace.h \
+ /home/developer/.systemc/include/sysc/communication/sc_clock.h \
+ /home/developer/.systemc/include/sysc/communication/sc_clock_ports.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_ports.h \
+ /home/developer/.systemc/include/sysc/communication/sc_event_finder.h \
+ /home/developer/.systemc/include/sysc/communication/sc_event_queue.h \
+ /home/developer/.systemc/include/sysc/communication/sc_export.h \
+ /home/developer/.systemc/include/sysc/communication/sc_fifo.h \
+ /home/developer/.systemc/include/sysc/communication/sc_fifo_ifs.h \
+ /home/developer/.systemc/include/sysc/communication/sc_fifo_ports.h \
+ /home/developer/.systemc/include/sysc/communication/sc_mutex.h \
+ /home/developer/.systemc/include/sysc/communication/sc_mutex_if.h \
+ /home/developer/.systemc/include/sysc/communication/sc_semaphore.h \
+ /home/developer/.systemc/include/sysc/communication/sc_semaphore_if.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_ports.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_resolved.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_resolved_ports.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_rv.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_lv.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_lv_base.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bv_base.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bit_proxies.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_proxy.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_signed.h \
+ /home/developer/.systemc/include/sysc/datatypes/misc/sc_value_base.h \
+ /home/developer/.systemc/include/sysc/utils/sc_temporary.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_length_param.h \
+ /home/developer/.systemc/include/sysc/datatypes/fx/sc_context.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_nbexterns.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_unsigned.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_int_base.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_uint_base.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_rv_ports.h \
+ /home/developer/.systemc/include/sysc/communication/sc_signal_rv.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bit.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_logic.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bv.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_lv.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_bigint.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_biguint.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_int.h \
+ /home/developer/.systemc/include/sysc/datatypes/int/sc_uint.h \
+ /home/developer/.systemc/include/sysc/datatypes/misc/sc_concatref.h \
+ /home/developer/.systemc/include/sysc/datatypes/bit/sc_bv.h \
+ /home/developer/.systemc/include/sysc/tracing/sc_trace.h \
+ /home/developer/.systemc/include/sysc/utils/sc_iostream.h \
+ /home/developer/.systemc/include/sysc/utils/sc_utils_ids.h \
+ /home/developer/.systemc/include/sysc/utils/sc_pvector.h \
+ /home/developer/.systemc/include/sysc/utils/sc_vector.h \
+ /home/developer/.systemc/include/sysc/packages/boost/utility/enable_if.hpp
+
+/home/developer/.systemc/include/systemc.h:
+
+/home/developer/.systemc/include/systemc:
+
+/home/developer/.systemc/include/sysc/kernel/sc_cmnhdr.h:
+
+/home/developer/.systemc/include/sysc/packages/boost/config.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/user.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/select_compiler_config.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/compiler/gcc.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/select_stdlib_config.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/stdlib/libstdcpp3.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/select_platform_config.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/platform/linux.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/posix_features.hpp:
+
+/home/developer/.systemc/include/sysc/packages/boost/config/suffix.hpp:
+
+/home/developer/.systemc/include/sysc/kernel/sc_externs.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_except.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_module.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_kernel_ids.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_report.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_except.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_report_handler.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_process.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_iostream.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_constants.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_object.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_attribute.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_export.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_communication_ids.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_interface.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_module_name.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_sensitive.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_time.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_nbdefs.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_cmnhdr.h:
+
+/home/developer/.systemc/include/sysc/datatypes/fx/scfx_ieee.h:
+
+/home/developer/.systemc/include/sysc/datatypes/fx/sc_fxdefs.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_machine.h:
+
+/home/developer/.systemc/include/sysc/packages/boost/detail/endian.hpp:
+
+/home/developer/.systemc/include/sysc/datatypes/fx/sc_fx_ids.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_nbutils.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bit_ids.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_int_ids.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_wait.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_simcontext.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_status.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_hash.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_pq.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_wait_cthread.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_logic.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_macros.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_mempool.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bit.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_ifs.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_writer_policy.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_process_handle.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_module.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_list.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_simcontext.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_process_handle.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_ver.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_buffer.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_port.h:
+
+/home/developer/.systemc/include/sysc/kernel/sc_event.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_prim_channel.h:
+
+/home/developer/.systemc/include/sysc/tracing/sc_trace.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_clock.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_clock_ports.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_ports.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_event_finder.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_event_queue.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_export.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_fifo.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_fifo_ifs.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_fifo_ports.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_mutex.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_mutex_if.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_semaphore.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_semaphore_if.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_ports.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_resolved.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_resolved_ports.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_rv.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_lv.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_lv_base.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bv_base.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bit_proxies.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_proxy.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_signed.h:
+
+/home/developer/.systemc/include/sysc/datatypes/misc/sc_value_base.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_temporary.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_length_param.h:
+
+/home/developer/.systemc/include/sysc/datatypes/fx/sc_context.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_nbexterns.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_unsigned.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_int_base.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_uint_base.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_rv_ports.h:
+
+/home/developer/.systemc/include/sysc/communication/sc_signal_rv.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bit.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_logic.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bv.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_lv.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_bigint.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_biguint.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_int.h:
+
+/home/developer/.systemc/include/sysc/datatypes/int/sc_uint.h:
+
+/home/developer/.systemc/include/sysc/datatypes/misc/sc_concatref.h:
+
+/home/developer/.systemc/include/sysc/datatypes/bit/sc_bv.h:
+
+/home/developer/.systemc/include/sysc/tracing/sc_trace.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_iostream.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_utils_ids.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_pvector.h:
+
+/home/developer/.systemc/include/sysc/utils/sc_vector.h:
+
+/home/developer/.systemc/include/sysc/packages/boost/utility/enable_if.hpp:
